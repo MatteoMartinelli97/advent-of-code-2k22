d53688fcbe15dc96fa07ab6d1d36895756020df0
Matteo Martinelli <teo.martinelli97@gmail.com>
2022-12-04 20:58:32 +0100

    prettier day 4 version

--- day4.py
+++ day4.py
@@ -6,23 +6,23 @@ import numpy as np
 file_in = Path('input') / 'day4.txt'
 
 ranges_couple = file_in.open().read().splitlines()
-ranges = [(r.split(',')[0], r.split(',')[1]) for r in ranges_couple]
+ranges = [r.split(',') for r in ranges_couple]
 
 # Part 1
-def check_total_overlapping(r1: str, r2: str) -> bool:
+def is_total_overlap(r1: str, r2: str) -> bool:
     r1_min, r1_max = (int(x) for x in r1.split('-'))
     r2_min, r2_max = (int(x) for x in r2.split('-'))
     
     return ((r1_min <= r2_min) & (r1_max >= r2_max)) or ((r1_min >= r2_min) & (r1_max <= r2_max))
 
-a = [check_total_overlapping(r[0], r[1]) for r in ranges]
-print(sum(a))
+total_overlap = [is_total_overlap(r[0], r[1]) for r in ranges]
+print(sum(total_overlap))
 
 # Part 2
-def check_partial_overlapping(r1: str, r2: str) -> bool:
+def is_partial_overlap(r1: str, r2: str) -> bool:
     r1_min, r1_max = (int(x) for x in r1.split('-'))
     r2_min, r2_max = (int(x) for x in r2.split('-'))
-    return (r1_min in range(r2_min, r2_max+1) or r1_max in range(r2_min, r2_max+1)) or (r2_min in range(r1_min, r1_max+1) or r2_max in range(r1_min, r1_max+1))
+    return r2_min <= r1_min <= r2_max or r1_min <= r2_min <= r1_max 
 
-a = [check_partial_overlapping(r[0], r[1]) for r in ranges]
-print(sum(a))
+partial_overlap = [is_partial_overlap(r[0], r[1]) for r in ranges]
+print(sum(partial_overlap))
