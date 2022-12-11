b0f3fe03acaf4b78bb9cb6fe13eae031c3f10199
Matteo Martinelli <teo.martinelli97@gmail.com>
2022-12-11 21:50:19 +0100

    day6, maybe ugly

---
+++ day6.py
@@ -0,0 +1,22 @@
+# 6th day of Advent of Code 2022
+# Matteo Martinelli
+
+from pathlib import Path
+import numpy as np
+
+file_in = Path('input') / 'day6.txt'
+
+# pre-process
+datastream = file_in.open().read()
+
+# Part 1
+for i in range(len(datastream)):
+    if len(set(datastream[i:i+4])) == 4:
+        print(i+4)
+        break
+
+# Part 2
+for i in range(len(datastream)):
+    if len(set(datastream[i:i+14])) == 14:
+        print(i+14)
+        break
